20a0a0a6aa406091af52462e761020b6e8769347
Muhammad Arif <talentedarif@gmail.com>
2022-01-05 00:31:11 +0500

    meta data
    fetching meta data

--- index.php
+++ index.php
@@ -4,11 +4,25 @@
     <?php while(have_posts()) { ?>
         <?php the_post(); ?>
             <h2>
-                <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+                <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
             </h2>
+            <div>
+            Posted On
+            <a href="<?php echo get_permalink() ?>">
+                <time datetime="<?php echo get_the_date('c'); ?>"><?php echo get_the_date(); ?></time>
+            </a>
+            By <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' )) ?>"><?php echo get_the_author(); ?></a>
+            </div>
+            <div>
+                <?php the_excerpt(); ?>
+                <a href="<?php get_the_permalink() ?>" title="<?php the_title_attribute(); ?>">
+                Read More... <span class="u-screen-reader-text">About <?php the_title(); ?></span>
+            </a>
+            </div>
         <?php } ?>
+        <?php the_posts_pagination(); ?>
     <?php } else { ?>
-        <p>Sorry</p>
+        <p>Sorry, No Posts Matched Criteria</p>
 <?php } ?>
 
 <?php get_footer(); ?>
