0306150c35e90606d11386451cd7a939d7bbc72b
Muhammad Arif <talentedarif@gmail.com>
2022-01-05 00:01:50 +0500

    fetching posts
    fetching all posts

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php get_header(); ?>
 
+<?php if(have_posts()) { ?>
+    <?php while(have_posts()) { ?>
+        <?php the_post(); ?>
+            <h2>
+                <a href="<?php the_permalink() ?>"><?php the_title(); ?></a>
+            </h2>
+        <?php } ?>
+    <?php } else { ?>
+        <p>Sorry</p>
+<?php } ?>
 
 <?php get_footer(); ?>
