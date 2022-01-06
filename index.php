1f88367093f5bd43d2b1b576ec34e23c11bde9a8
Muhammad Arif <talentedarif@gmail.com>
2022-01-07 00:57:27 +0500

    files done
    function and lib/helpers file done

--- index.php
+++ index.php
@@ -7,17 +7,13 @@
                 <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
             </h2>
             <div>
-            Posted On
-            <a href="<?php echo get_permalink() ?>">
-                <time datetime="<?php echo get_the_date('c'); ?>"><?php echo get_the_date(); ?></time>
-            </a>
-            By <a href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' )) ?>"><?php echo get_the_author(); ?></a>
+                <?php firstthemes_post_meta(); ?>
             </div>
             <div>
                 <?php the_excerpt(); ?>
-                <a href="<?php get_the_permalink() ?>" title="<?php the_title_attribute(); ?>">
-                Read More... <span class="u-screen-reader-text">About <?php the_title(); ?></span>
-            </a>
+
+                <?php firstthemes_readmore_link(); ?>
+
             </div>
         <?php } ?>
         <?php the_posts_pagination(); ?>
---
+++ lib/helpers.php
@@ -0,0 +1,14 @@
+<?php 
+    function firstthemes_post_meta(){
+        echo 'Posted On';
+        echo '<a href="' . get_permalink( ) . '">';
+        echo '<time datetime="' . get_the_date('c') .'">' . get_the_date(). '</time>';
+        echo '</a>';
+        echo 'By <a href="' . get_author_posts_url( get_the_author_meta( 'ID' )) . '">' . get_the_author( ) . '</a>';
+    }
+    function firstthemes_readmore_link(){
+        echo '<a href="' .  get_the_permalink() . '" title="' . the_title_attribute(['echo' => false]) . '">';
+        echo 'Read More... <span class="u-screen-reader-text">About' . get_the_title() . '</span>';
+        echo '</a>';
+    }
+?>
