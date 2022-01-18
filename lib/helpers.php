1d48e4dad18ddf49eabdfc38868107d0bf33f990
Muhammad Arif <talentedarif@gmail.com>
2022-01-19 00:36:55 +0500

    update helper

--- lib/helpers.php
+++ lib/helpers.php
@@ -1,14 +1,30 @@
 <?php 
     function firstthemes_post_meta(){
-        echo 'Posted On';
-        echo '<a href="' . esc_url(get_permalink( )) . '">';
-        echo '<time datetime="' . esc_attr(get_the_date('c')) .'">' . esc_html(get_the_date()). '</time>';
-        echo '</a>';
-        echo 'By <a href="' . esc_url(get_author_posts_url( get_the_author_meta( 'ID' ))) . '">' . esc_html(get_the_author( )) . '</a>';
+        // Translator %s Post date
+        printf(
+            esc_html__('Posted on %s', 'firstthemes'),
+            '<a href="' . esc_url(get_permalink( )) . '"><time datetime="' . esc_attr(get_the_date('c')) .'">' . esc_html(get_the_date()). '</time></a>'
+        );
+        printf(
+            // Translator %s Post Author
+            esc_html__(' By %s','firstthemes'),
+            '<a href="' . esc_url(get_author_posts_url( get_the_author_meta( 'ID' ))) . '">' . esc_html(get_the_author( )) . '</a>'
+        );
     }
     function firstthemes_readmore_link(){
         echo '<a href="' .  esc_url(get_the_permalink()) . '" title="' . the_title_attribute(['echo' => false]) . '">';
-        echo 'Read More... <span class="u-screen-reader-text">About' . get_the_title() . '</span>';
+        // Translator %s Post title
+        printf(
+            wp_kses(
+            __('Read More <span class="u-screen-reader-text">About %s</span>', 'firstthemes'),
+            [
+                'span' => [
+                    'class' => []
+                ]
+            ]
+        ),
+            get_the_title()
+        );
         echo '</a>';
     }
 ?>
